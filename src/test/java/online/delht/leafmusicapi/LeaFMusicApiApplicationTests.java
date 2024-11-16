52bd486dae318cd5f015820a65692ba6c95710ac
Thuan <thuanluong1230@gmail.com>
2024-11-16 23:59:14 +0700

    Initial commit

---
+++ src/test/java/online/delht/leafmusicapi/LeaFMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.delht.leafmusicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaFMusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
