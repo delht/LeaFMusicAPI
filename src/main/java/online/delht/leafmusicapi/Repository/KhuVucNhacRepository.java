ebc36845daad4720e28bf5492ffd494707634873
Thuan <thuanluong1230@gmail.com>
2024-11-17 09:26:13 +0700

    Chuyển kết nối dtb railway sang local để tiết kiệm tài nguyên

--- src/main/java/online/delht/leafmusicapi/Repository/KhuVucNhacRepository.java
+++
@@ -1,9 +0,0 @@
-package online.delht.leafmusicapi.Repository;
-
-import online.delht.leafmusicapi.Entity.KhuVucNhac;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KhuVucNhacRepository extends JpaRepository<KhuVucNhac, Integer> {
-}
